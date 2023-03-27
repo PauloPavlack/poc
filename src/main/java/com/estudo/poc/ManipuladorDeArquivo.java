f308e6a0fcd043672492c1a1c83eeccc93857efa
Paulo Pavlack <paulo.pavlack.dev@gmail.com>
2023-03-26 23:09:32 -0300

    Adicionados metodos para manipulação de arquivos

--- src/main/java/com/estudo/poc/ManipuladorDeArquivo.java
+++ src/main/java/com/estudo/poc/ManipuladorDeArquivo.java
@@ -1,5 +1,50 @@
 package com.estudo.poc;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class ManipuladorDeArquivo {
 
+	public String criarArquivo() {
+
+		// Cria o objeto File para representar o arquivo
+		File arquivo = new File("meuarquivo.txt");
+
+		try {
+			// Cria o objeto PrintWriter para escrever no arquivo
+			PrintWriter writer = new PrintWriter(arquivo);
+
+			// Escreve algumas linhas no arquivo usando o método println
+			writer.println("Linha 1");
+			writer.println("Linha 2");
+			writer.println("Linha 3");
+
+			// Fecha o objeto PrintWriter para garantir que todos os dados sejam gravados no
+			// arquivo
+			writer.close();
+
+			System.out.println("Arquivo criado e escrito com sucesso!");
+			return "Arquivo criado e escrito com sucesso!";
+		} catch (Exception e) {
+			// Trata qualquer exceção que possa ocorrer
+			System.out.println("Ocorreu um erro ao criar ou escrever no arquivo: " + e.getMessage());
+			return "Ocoreu um problema";
+		}
+	}
+
+	public String lerArquivo() {
+		StringBuilder sb = new StringBuilder();
+		try (BufferedReader br = new BufferedReader(new FileReader("meuarquivo.txt"))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				sb.append(line);
+			}
+		} catch (IOException e) {
+			System.err.println("Erro ao ler arquivo: " + e);
+		}
+		return sb.toString();
+	}
 }
--- src/main/java/com/estudo/poc/PocController.java
+++ src/main/java/com/estudo/poc/PocController.java
@@ -3,13 +3,27 @@ package com.estudo.poc;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
 public class PocController {
 
-    @GetMapping("test")
-    public ResponseEntity<String> get(){
-        return new ResponseEntity<>("teste", HttpStatus.OK);
-    } 
+	@GetMapping("test")
+	public ResponseEntity<String> get() {
+		return new ResponseEntity<>("teste", HttpStatus.OK);
+	}
+
+	@PutMapping("criar-arquivo")
+	public String criarArquivo() {
+		ManipuladorDeArquivo manipulador = new ManipuladorDeArquivo();
+		return manipulador.criarArquivo();
+	}
+
+	@PutMapping("ler-arquivo")
+	public String lerArquivo() {
+		ManipuladorDeArquivo manipulador = new ManipuladorDeArquivo();
+		return manipulador.lerArquivo();
+	}
+
 }
